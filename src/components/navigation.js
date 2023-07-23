727fbb6f87ce645216e945f71483e5e162214450
Herbert Henrique <herbert.henrique@sumup.com>
2023-07-23 11:56:51 +0200

    chore: update all libs

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,10 +2,12 @@ import React from "react"
 import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
-export default (props) => (
+const Navigation =  () => (
   <nav className="navigation"> 
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
   
 )
+
+export default Navigation;
